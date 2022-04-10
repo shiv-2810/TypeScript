648d7934b47aa06032acc552a806c0c81041a0a4
Shivam Kumar <shivam.kumar@hungerbox.com>
2022-04-10 09:50:47 +0530

    Typescript done

---
+++ objs-array-enums.ts
@@ -0,0 +1,42 @@
+// // var person1 :{
+// //     name: string;
+// //     age: number;
+// // } = {
+//  var person1= {   
+//     name: 'Shivam',
+//     age : 20
+// };
+
+// let hobbie :any[];    // this is a special type of array in wich we can store any type of data.
+// hobbie =['sports',1]
+// console.log(person1.name);
+
+
+
+// var student :{
+//   name:string;
+//   age:number;
+//   hobbies:string[];
+//   role:[number,string]; 
+// } = {
+//     name: 'Shivam',
+//     age: 30,
+//     hobbies:['Cricket','Badminton'],
+//     role:[2,'author'] // tuple 
+// }
+
+// ENUM
+
+// enum Role {ADMIN,READ_ONLY,AUTHOR};
+
+// const person = {
+//     name: 'Shivam',
+//     age: 30,
+//     hobbies:['Cricket','Badminton'],
+//     role:Role.ADMIN
+
+// }
+
+
+
+
