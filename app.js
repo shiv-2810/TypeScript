e9d60c5ec26f816cfefe34f842bf11ccb4644842
Shivam Kumar <shivam.kumar@hungerbox.com>
2022-04-10 07:11:31 +0530

    index.html is added

---
+++ app.js
@@ -0,0 +1,5 @@
+var person1 = {
+    name: 'Shivam',
+    age: 20
+};
+console.log(person1.name);
---
+++ app.ts
@@ -0,0 +1,12 @@
+// var person1 :{
+//     name: string;
+//     age: number;
+// } = {
+ var person1= {   
+    name: 'Shivam',
+    age : 20
+};
+
+let hobbie :any[];    // this is a special type of array in wich we can store any type of data.
+hobbie =['sports',1]
+console.log(person1.name);
